980f61093a9f09331ac166d31c2ab0af3e1c37a8
yanganyun <237364436@qq.com>
2018-05-30 11:01:03 +0800

    修改

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
-import { 
-  Pagination, Menu,Radio,Checkbox,CheckboxGroup,Select,Alert,Table,TableColumn,Button
-} from 'element-ui'
+import Element from 'element-ui'
 import '~/assets/scss/plugin/element-variables.scss';
-Vue.use(Pagination);
-Vue.use(Menu);
-Vue.use(Radio);
-Vue.use(Checkbox);
-Vue.use(CheckboxGroup);
-Vue.use(Select);
-Vue.use(Alert);
-Vue.use(Table);
-Vue.use(TableColumn);
-Vue.use(Button);
+import locale from 'element-ui/lib/locale/lang/en'
+Vue.use(Element, { locale })
+
+
+// import Vue from 'vue'
+// import { 
+//   Pagination, Menu,Radio,Checkbox,CheckboxGroup,Select,Alert,Table,TableColumn,Button
+// } from 'element-ui'
+// import '~/assets/scss/plugin/element-variables.scss';
+// Vue.use(Pagination);
+// Vue.use(Menu);
+// Vue.use(Radio);
+// Vue.use(Checkbox);
+// Vue.use(CheckboxGroup);
+// Vue.use(Select);
+// Vue.use(Alert);
+// Vue.use(Table);
+// Vue.use(TableColumn);
+// Vue.use(Button);
--- plugins/ga.js
+++ plugins/ga.js
@@ -41,35 +41,4 @@ export default ({ app }) => {
     gtag('js', new Date());
     gtag('config', 'UA-107010673-1');
   }
-
-  //检测线上环境，或者 检测测试环境使用ga（在nuxt.config里配置在测试环境是否显示ga）
-  // if (NODE_ENV !== 'production' && !buildData.publicPath || NODE_ENV == 'production' && !buildData.testGa && !buildData.publicPath) {
-  //   //测试环境支付key
-  //   window.payCode = "pk_test_ymxnY3KoqRcjCEElfvFxPy1G"
-    
-  //   //防止ga报错，默认添加ga方法
-  //   window.ga = function(){
-  //     console.log('this is test! no ga code!');
-  //   };
-  //   window._gat = {
-  //     _getTracker(){
-  //       return {_addTrans(){},_addItem(){},_trackTrans(){}};
-  //     }
-  //   }
-  //   //跳出，不走ga
-  //   return;
-  // }else{
-  //   //线上环境支付key
-  //   window.payCode = "pk_live_mRSdUvgwE4pZo2IVofL4cVch"
-  // }
-
-  
-
-
-  // ga('create', 'UA-XXXXXXXX-X', 'auto');
-
-  // app.router.afterEach((to, from) => {
-  //   ga('set', 'page', to.fullPath)
-  //   ga('send', 'pageview')
-  // })
 }
