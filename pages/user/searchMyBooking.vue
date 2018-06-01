64b9f4dc5dce58935529e1a23d02d7c5aee37774
yanganyun <237364436@qq.com>
2018-06-01 22:06:17 +0800

    优化

--- pages/user/searchMyBooking.vue
+++ pages/user/searchMyBooking.vue
@@ -8,11 +8,11 @@
 			<p class="top_tip">Please enter reservee's following<br>information to view the order status</p>
 			<ul class="search_info">
 				<li class="user_name">
-					<input class="input validate" type="text" v-model="lastName" placeholder="First name">
-					<input class="input validate" type="text" v-model="firstName" placeholder="Last name">
+					<input class="input validate" type="text" vType="enName" v-model="lastName" placeholder="First name">
+					<input class="input validate" type="text" vType="enName" v-model="firstName" placeholder="Last name">
 				</li>
 				<li>
-					<input class="input validate" type="text" v-model="email" placeholder="Email">
+					<input class="input validate" type="text" vType="text" v-model="email" placeholder="Email">
 				</li>
 				<li>
 					<span class="btn" @click="search">Submit</span>
@@ -24,7 +24,7 @@
 			<div class="login_box">
 				<h2>Login</h2>
 				<p>If you have already logged in via your <br>Facebook account and placed an order, you can login <br>through Facebook and view the status of your order</p>
-				<span class="btn"><i class="iconfont">&#xe613;</i>Log in with Facebook</span>
+				<span class="btn" @click="facebookLogin"><i class="iconfont">&#xe613;</i>Log in with Facebook</span>
 			</div>
 
 		</div>
@@ -33,7 +33,7 @@
 </template>
 
 <script>
-	import {lpValidate} from "~/assets/js/main.js"
+	import Validate from "~/assets/js/panda/validate.js"
 
 	export default {
 		name: 'searchMyBooking',
@@ -65,30 +65,153 @@
 					"emailAddress": this.email
 				};
 
+				//校验
+				if(this.validate.validate()){
+					self.axios.post("https://api.localpanda.com/api/activity/order/list", JSON.stringify(postData), {
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					}).then(function(response) {
+						if(response.data.length>0){
+							window.localStorage.setItem("obj",JSON.stringify(postData));
+							window.location.href="/user/myBookings?menu=0&flag=1"
+						}else{
+							self.showErrorText = true;
+						}
+					}, function(response) {})
+				}
 				
+				
+			},
 
-				return;
-				self.axios.post("https://api.localpanda.com/api/activity/order/list", JSON.stringify(postData), {
-					headers: {
-						'Content-Type': 'application/json'
+			ChangeCallback(response) {
+				if(response.status === 'connected') {
+
+				} else if(response.status === 'not_authorized') { //未经授权
+					FB.logout(function(response) {})
+				} else {
+					//console.log('不是登陆到Facebook;不知道是否授权');
+				}
+			},
+			fbAsyncInit() {
+				let that = this
+				FB.init({
+					appId: '487179281653793',
+					cookie: true,
+					xfbml: true,
+					version: 'v2.10'
+				});
+				FB.AppEvents.logPageView();
+
+				// check on loading
+				FB.getLoginStatus(function(response) {
+					that.ChangeCallback(response);
+				})
+				that.loadJs=true
+				
+			},
+			loadScript(url, callback,Fn) {
+				var that=this
+				var script = document.createElement('script');
+				var head = document.getElementsByTagName('head')[0];
+				var loaded;
+				script.src = url;
+				if(typeof callback === 'function'){
+						script.onload = script.onreadystatechange = function(){
+								if(!loaded && (!script.readyState || /loaded|complete/.test(script.readyState))){
+										script.onload = script.onreadystatechange = null;
+										loaded = true;
+										callback();
+										if(that.loadJs==true){
+											Fn()
+										}
+								}
+						}
+				}
+				head.appendChild(script);
+			},
+			facebookLogin(){
+				let that = this
+				that.loadScript("//connect.facebook.net/en_US/sdk.js",that.fbAsyncInit,that.logIn)
+			},
+			logIn(){
+				let that=this
+				FB.login(function(response) {
+					/*console.log("login"+response)*/
+					if(response.authResponse) {
+						that.checkLoginState()
 					}
-				}).then(function(response) {
-					if(response.data.length>0){
-						window.localStorage.setItem("obj",JSON.stringify(postData));
+				});
+			},
+			checkLoginState() {
+				let that = this
+				FB.getLoginStatus(function(response) {
+					that.statusChangeCallback(response);
+					//location.reload()
+				})
+			},
+			statusChangeCallback(response) {
+				const that = this
+				if(response.status === 'connected') { //登陆状态已连接
+					that.fbToken = response.authResponse.accessToken;
+					that.faceUserID = response.authResponse.userID;
+					/*console.log(response.authResponse.userID )
+					console.log(that.faceUserID)*/
+					var Istrue = false
+					//获取用户信息
+					FB.api('/me?fields=name,picture', function(response) {
+						Istrue = true
+						that.logImg = response.picture.data.url
+						
+						window.localStorage.setItem("user_photo", that.logImg)
+						that.islogIn = 1;
+
+						window.localStorage.setItem("logstate", that.islogIn);
+						window.localStorage.setItem("fbToken", that.fbToken);
+						
+						that.getToken(Istrue)
+					});
+
+					//setTimeout(function(){history.go(0)},300)
+				} else if(response.status === 'not_authorized') { //未经授权
+					FB.logout(function(response) {})
+				} else {
+					//console.log('不是登陆到Facebook;不知道是否授权');
+				}
+			},
+			getToken(Istrue) {
+				if(Istrue == true) {
+					let $this = this;
+					let obj = {
+						accessToken: $this.fbToken,
+						userId: $this.faceUserID
+					}
+					this.axios.post("https://api.localpanda.com/api/user/login/facebook", JSON.stringify(obj), {
+						headers: {
+							'Content-Type': 'application/json; charset=UTF-8'
+						}
+					}).then(function(response) {
+						/*console.log("getToken"+response)*/
+						
+						$this.userid = response.data.response
+						
+						window.localStorage.setItem("userid", $this.userid)
+						
 						window.location.href="/user/myBookings?menu=0&flag=1"
-					}else{
-						self.errorText="Sorry, we didn't find an order under this name and email"	
-					}
-					
-				}, function(response) {})
+						
+
+					}, function(response) {})
+				}
 			}
+			
 		},
 		mounted(){
-			var aaa = new lpValidate({
-				el:'.validate',
-				errorClass:'valError'
+			//调用校验组件
+			this.validate = new Validate({
+				inputClassName:'validate',
+				errorClassName:'valError'
 			});
-			console.log(aaa);
+			
 		}
 	}
 </script>
@@ -140,9 +263,12 @@
 					margin-left: -1px;
 				}
 			}
-			.valError{
-				border-color: #e14f3f;
-			}
+			
+		}
+		.valError{
+			border-color: #e14f3f;
+			position: relative;
+			z-index: 2;
 		}
 	}
 	.search_tip{
