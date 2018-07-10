171ddc949c6c0dbc9613493eaef07ba4aa0a6d13
yanganyun <237364436@qq.com>
2018-07-10 09:57:17 +0800

    优化

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ module.exports = {
   },
   build: {
     publicPath: 'https://resource.localpanda.cn/static/js/',
-    testGa: true,
-     payCode: 'pk_live_mRSdUvgwE4pZo2IVofL4cVch',
+    // testGa: true,
+    //  payCode: 'pk_live_mRSdUvgwE4pZo2IVofL4cVch',
     // publicPath: '/mobile/_nuxt/', //PC和移动共存区分用
 
 
