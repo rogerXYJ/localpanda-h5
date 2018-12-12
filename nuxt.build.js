68825584a7fa89076905959c7d8d2cb9b7961175
yanganyun <237364436@qq.com>
2018-12-12 17:11:18 +0800

    xx

--- nuxt.build.js
+++ nuxt.build.js
@@ -1,4 +1,6 @@
 module.exports = {
   publicPath: 'https://cloud.localpanda.com/static/js/',
-  testGa: true
+  testGa: true,
+  payCode: 'pk_live_mRSdUvgwE4pZo2IVofL4cVch',
+  paypalCode: 'production'
 };
